05e1cccee2428e73724f047bf4dcd8e9482823aa
Michael Jay Arizala <michaeljayarizala@agusandelnorte.gov.ph>
2024-10-09 17:00:00 +0800

    enhance script to set block ranges as command options

--- bored-ape.py
+++ bored-ape.py
@@ -8,12 +8,18 @@ django.setup()
 
 from core.contracts import CoreContract
 
-def main():
+def main(args):
     contract = CoreContract()
     contract.contract_abi_file = "core/abis/TransferABI.json"
     contract.contract_address_env = "BAYC_CONTRACT_ADDRESS"
-    contract.get_transfers(from_block=20925802)
+    contract.get_transfers(from_block=args.start, to_block=args.end if args.end else 'latest')
 
 if __name__ == "__main__":
-    
-    main()
+
+    cmd = argparse.ArgumentParser(description="Fetch transfer events and persist filter result to database.")
+    cmd.add_argument("--start", type=int, required=True, help="Start of events range to listen to.")
+    cmd.add_argument("--end", type=int, required=False, help="End of events range to listen to. Defaults to 'latest' if not specified.")
+
+    cmd_args = cmd.parse_args()
+
+    main(cmd_args)
